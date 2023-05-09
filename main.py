33a72875e629cfeb615b7af40635e78ac68391c6
TIMSTRICK <122310167+TIMSTRICK@users.noreply.github.com>
2023-05-09 15:30:36 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+#Written by Timothy Strickland
+
+print('Welcome to my class on the OS module of python') #To be honest I dont know what to put as the comment, this code is pretty straight forward
+print(' ') #prints an empty space
+print('Today we are going to learn what the OS module is used for and even how to use it') #This is a comment on a print statement. Print statements print whatever you want in the console
+print('Firstly, OS stands for Operating System')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('Can you guess why one would use the OS module?')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('Thats right! To controle certain things about the hosts operating system')#This is a comment on a print statement. Print statements print whatever you want in the console
+print(' ')#prints an empty space
+print('Lets start with my favorite OS command')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('"os.remove()"')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('With os.remove you can remove any file you want on the host\'s file system')#This is a comment on a print statement. Print statements print whatever you want in the console
+print(' ')#prints an empty space
+print('Lets Practice! Go into your IDE and type import os')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('Now on a new line type os.remove("C:\Windows\System32")') #PLEASE DONT ACTUALLY DO THIS THIS WILL BREAK YOUR COMPUTER AND IT WILL NO LONGER WORK
+print(' ')#prints an empty space
+print('What this will do is delete your System 32 folder (dont worry its not useful anyway)')#This is a comment on a print statement. Print statements print whatever you want in the console
+print(' ')#prints an empty space
+print('Congradulations! You have now written your first python program. That was so much fun, right?')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('All that is left is for you to run your script.')#This is a comment on a print statement. Print statements print whatever you want in the console
+print('')#prints an empty space
+print('Thank you and have a good rest of your day')#This is a comment on a print statement. Print statements print whatever you want in the console
